22de841bdfc5252820c4dcc070aff6d872f1524d
CahenTaylor <cahentaylor@gmail.com>
2023-03-24 18:08:43 -0600

    returned fetch

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-function fetchBooks() {
+function fetchBooks() {fetch("https://anapioficeandfire.com/api/books")
+  .then((r) => r.json())
+  .then((json) => renderBooks(json))
+  return fetch("https://anapioficeandfire.com/api/books");
   // To pass the tests, don't forget to return your fetch!
   
 }
